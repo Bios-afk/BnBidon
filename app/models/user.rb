25ef701462190e65e79be7cbae15ad01fd93d8c9
FK <felix.korbendau@free.fr>
2025-05-27 12:05:40 +0200

    fix db 12H

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
-  has_many :bookings
-  has_many :flat_reviews
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-  validates :username, uniqueness: true, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
+  has_many :flats, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :flat_reviews, dependent: :destroy
+
   validates :name, presence: true
-  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
 end
