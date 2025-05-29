1b819db975b10f9f921212ee22c06a7fa707193f
FK <felix.korbendau@free.fr>
2025-05-29 12:29:47 +0200

    form add up

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -6,8 +6,18 @@ export default class extends Controller {
     console.log("connecté au modal controller");
     this.modal = new bootstrap.Modal(this.element);
     console.log(this.modal);
-    addEventListener("turbo:before-stream-render", (event) => {
-      this.modal.hide();
+    addEventListener("turbo:submit-end", (event) => {
+      console.log(event);
+      if (event.detail.success) {
+        this.modal.hide();
+      }
+    });
+
+    addEventListener("hide.bs.modal", (event) => {
+      this.element.querySelectorAll(".is-invalid").forEach((el) => {
+        el.classList.remove("is-invalid");
+      });
+      this.element.querySelector('.alert')?.remove();
     });
   }
 }
