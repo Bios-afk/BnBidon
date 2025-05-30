49d85e42b1670436d21cf6bd615753f9bb7b7f60
Thibault Hutin <fata_daoc@hotmail.com>
2025-05-30 16:01:34 +0200

    cards ok pour le dashboard

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -5,6 +5,6 @@ module BookingsHelper
     total_price = nights * price_per_night
 
     "#{number_to_currency(total_price, unit: '€', separator: ',', delimiter: ' ', format: "%n %u")}" \
-    "<small class='text-muted'>(#{nights} nuits × #{number_to_currency(price_per_night, unit: '€', separator: ',', delimiter: ' ', format: "%n %u")})</small>".html_safe
+    "<small class='text-muted'>(#{nights} nights × #{number_to_currency(price_per_night, unit: '€', separator: ',', delimiter: ' ', format: "%n %u")})</small>".html_safe
   end
 end
